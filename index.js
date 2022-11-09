e88fda7f2cfebd17c42223aaaa7f76a825074267
Anas <anasmahmud523@gmail.com>
2022-11-09 18:58:35 +0600

    query by email

--- index.js
+++ index.js
@@ -34,7 +34,12 @@ async function run() {
 
         // reviews api
         app.get('/myReview', async (req, res) => {
-            const query = {};
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
             const cursor = reviewCollection.find(query);
             const myReview = await cursor.toArray();
             res.send(myReview);
