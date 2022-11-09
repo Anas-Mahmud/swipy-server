19e08c0a8ae5352fbc03d9ef99b90e825e6aa68d
Anas <anasmahmud523@gmail.com>
2022-11-09 17:11:05 +0600

    review post

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('swipy').collection('services');
+        const reviewCollection = client.db('swipy').collection('reviews');
 
+        // services api
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
@@ -28,6 +30,13 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query)
             res.send(service);
+        })
+
+        // reviews api
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
         })
     }
     finally {
