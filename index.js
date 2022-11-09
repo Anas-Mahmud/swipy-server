52495c4cd1918c34f47d687a9405ed5aae914981
Anas <anasmahmud523@gmail.com>
2022-11-09 23:16:03 +0600

    delete review

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
             const result = await reviewCollection.insertOne(review);
             res.send(result);
         })
+
+        app.delete('/myReview/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
